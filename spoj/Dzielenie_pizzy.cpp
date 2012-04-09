7e9b363bfb807cecc06d60101bd5d6e03c78b5c3
rafal <ravik1222@gmail.com>
2012-04-09 21:05:21 +0200

    dzielenie pizzy

---
+++ spoj/Dzielenie_pizzy.cpp
@@ -0,0 +1,44 @@
+// za wolne - obadaj fstream
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int d;
+int n;
+double odl;
+int ciecia;
+
+void read()
+{
+	cin >> d;
+	cin >> n;
+}
+
+void tnij()
+{
+	if( n % 2 == 0 )
+		ciecia = n/2;
+	else
+	{
+		ciecia = n;
+	}
+	odl = (M_PI*d)/ (ciecia*2);
+	cout	<<odl<<" "
+		<<ciecia<<'\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	
+	int t;
+	cout << setprecision (3)
+		<<fixed;
+	cin >> t;
+	for( ; t > 0 ; t-- )
+	{
+		read();
+		tnij();
+	}
+}
