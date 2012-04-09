13662f9e4c0fbe9a0ba12ef12e4c2f31e23f350e
ravik122 <ravik1222@gmail.com>
2012-04-09 21:40:05 +0200

    dzielenie pizzy

--- spoj/Dzielenie_pizzy.cpp
+++ spoj/Dzielenie_pizzy.cpp
@@ -1,7 +1,5 @@
-//cout,cin za wolne trzeba bylo uzyc printf'a 
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 #include <stdio.h>
 using namespace std;
 
@@ -25,20 +23,14 @@ void tnij()
 		ciecia = n;
 	}
 	odl = (M_PI*d)/ (ciecia*2);
-/*	cout	<<odl<<" "
-		<<ciecia<<'\n';
-*/
 	printf ("%.3f %d\n",odl,ciecia);
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
 	
 	int t;
-	cout << setprecision (3)
-		<<fixed;
-	cin >> t;
+	scanf( "%d", &t );
 	for( ; t > 0 ; t-- )
 	{
 		read();
