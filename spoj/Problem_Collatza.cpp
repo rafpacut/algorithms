6b1717d7ba02b865a4d1ee79a1dc3d46ef594f65
ravik122 <ravik1222@gmail.com>
2012-04-29 14:08:14 +0200

    AC

--- spoj/Problem_Collatza.cpp
+++ spoj/Problem_Collatza.cpp
@@ -1,22 +1,32 @@
-//dla 3 i 567 nie dziala (za dlugo nie wiem czy daje rozwiazanie) ma wyjsc 7 i 61, chyba mialo byc jednak rekurencyjnie ;]
-
 #include <iostream>
 
 using namespace std;
 
-int main()
+int wynik;
+int n = 0;
+void algorithm()
 {
-	ios_base::sync_with_stdio(0);
-	int wynik;
-	int n = 0;
 	cin >> wynik;
 	while( wynik != 1 )
 	{
 		if( (wynik % 2) == 0 )
 			wynik /= 2;
 		else
-			3*wynik+1;
+			wynik = 3*wynik+1;
 		n++;
 	}
 	cout <<n<<'\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	int t;
+	cin >> t;
+	while ( t > 0 )
+	{
+		algorithm();
+		n = 0;
+		t --;
+	}
 }
