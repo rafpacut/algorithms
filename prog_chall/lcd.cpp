0aa1caa813587a58fd76f45ed4879c8a8bc2deb0
ravik122 <ravik1222@gmail.com>
2012-05-07 15:58:35 +0200

    Zmiana sposobu wyswietlania

--- prog_chall/lcd.cpp
+++ prog_chall/lcd.cpp
@@ -25,89 +25,51 @@ void read()
 	cin >> str;
 }
 
-void print()
+int main()
 {
+	read();
 	for( int j = 0 ; j < 5 ; j++ )
 	{
 		for( int i = 0 ; str[i] != '\0' ; i++ )
 		{
-			for( int u = 0 ; u < s ; u++ )
-				cout <<t[int(str[i])-int('0')][j];
+			if( t[int(str[i])-int('0')][j] == "-")
+			{
+				cout<<" ";
+				for( int g = 0 ; g < s ; g++ )cout<<'-';
+				cout<<" ";
+			}			
+			if( t[int(str[i])-int('0')][j] == " ")
+			{
+				cout<<" "
+				    <<" "
+				    <<" ";
+			}
+			if( t[int(str[i])-int('0')][j] == " |")
+			{
+				cout<<" ";
+				cout<<" ";
+				cout<<"|";
+			}
+			if( t[int(str[i])-int('0')][j] == "| ")
+			{
+				cout<<'|';
+				cout<<" ";
+				cout<<" ";
+			}
+			if( t[int(str[i])-int('0')][j] == "| |")
+			{
+				cout<<'|';
+				cout<<" ";
+				cout<<'|';
+			}
+			if( t[int(str[i])-int('0')][j] == "|")
+			{
+				cout<<" ";
+				cout<<'|';
+				cout<<" ";
+			}
 		}
-	}	
-}
-
-int main()
-{
-	read();
-//	print();
-for( int u = 0 ; str[u] != '\0' ; u++ ){
-	for( int i = 0 ; i < 5 ; i++ )
-	{
-			if( t[int(str[u])-int('0')][i] == "-" )
-			{
-				cout <<" ";
-				for( int u = 0 ; u < s ; u++ ) cout <<"-";
-				cout <<" \n";
-			}
-			else
-			{
-				if( t[int(str[u])-int('0')][i] == " |" )
-				{
-					for( int u = 0 ; u < s ; u++ ) 
-					{
-							for( int g = 0; g < s; g++ )
-							{
-								cout <<" ";
-							}
-							cout <<" |\n";
-					}
-				}
-				else
-				{
-					if( t[int(str[u])-int('0')][i] == "| " )
-					{
-						for( int u = 0 ; u < s ; u++ ) 
-						{
-							cout <<"| ";
-							for( int g = 0; g < s; g++ )
-							{
-								cout <<" ";
-							}
-							cout <<'\n';
-						}
-					}
-					else
-					{
-						if( t[int(str[u])-int('0')][i] == "| |" )
-						{
-							for( int u = 0 ; u < s ; u++ )
-							{
-								cout<<'|';
-								for( int g = 0 ; g < s ; g++ )
-								{
-									cout <<" ";
-								}
-								cout<<"|\n";	
-								if( u == (s/2) )
-								{
-									
-								}
-							}
-						}
-						else
-						{
-							if( t[int(str[u])-int('0')][i] == "|" )
-							{
-								cout << " \n"; 
-								for( int u = 0 ; u < s ; u++ ) cout <<" | \n";
-							}
-						}
-
-					}
-				}
-
-			}
-			}}
+		cout<<'\n';
+	}
 
 }
