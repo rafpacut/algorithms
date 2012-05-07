390493a733a1386294444606320031fb57482c53
ravik122 <ravik1222@gmail.com>
2012-05-07 16:33:15 +0200

    WORKING

--- prog_chall/lcd.cpp
+++ prog_chall/lcd.cpp
@@ -28,6 +28,8 @@ void read()
 int main()
 {
 	read();
+	int h = s-1;
+	int h2 = s-1;
 	for( int j = 0 ; j < 5 ; j++ )
 	{
 		for( int i = 0 ; str[i] != '\0' ; i++ )
@@ -37,39 +39,57 @@ int main()
 				cout<<" ";
 				for( int g = 0 ; g < s ; g++ )cout<<'-';
 				cout<<" ";
-			}			
+			}
 			if( t[int(str[i])-int('0')][j] == " ")
 			{
-				cout<<" "
-				    <<" "
-				    <<" ";
+				cout<<" ";
+				for( int g = 0 ; g < s ; g++ )cout<<" ";
+				cout<<" ";
 			}
 			if( t[int(str[i])-int('0')][j] == " |")
 			{
 				cout<<" ";
-				cout<<" ";
+				for( int g = 0 ; g < s ; g++ )cout<<" ";
 				cout<<"|";
 			}
 			if( t[int(str[i])-int('0')][j] == "| ")
 			{
 				cout<<'|';
-				cout<<" ";
+				for( int g = 0 ; g < s ; g++ )cout<<" ";
 				cout<<" ";
 			}
 			if( t[int(str[i])-int('0')][j] == "| |")
 			{
 				cout<<'|';
-				cout<<" ";
-				cout<<'|';
+				for( int g = 0 ; g < s ; g++ )cout<<" ";
+				cout<<"|";
 			}
 			if( t[int(str[i])-int('0')][j] == "|")
 			{
 				cout<<" ";
 				cout<<'|';
-				cout<<" ";
+				for( int g = 0 ; g < s ; g++ )cout<<" ";
 			}
 		}
 		cout<<'\n';
+		if( j == 1 )
+		{
+			while( h != 0 )
+			{
+				j--;
+				h--;
+				break;
+			}
+		}
+		if( j == 3 )
+		{
+			while ( h2 != 0 )
+			{
+				j--;
+				h2--;
+				break;
+			}
+		}
 	}
 
 }
