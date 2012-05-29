439f3758b1a2763ba95c0bc265326d71c5ee3faa
ravik122 <ravik1222@gmail.com>
2012-05-29 22:17:07 +0200

    Zmiana okreslania podciagu z liczb na cyfry

--- types.cpp
+++ types.cpp
@@ -1,4 +1,4 @@
-#define NDEBUG
+//#define NDEBUG
 
 #include <iostream>
 #include <cassert>
@@ -10,6 +10,7 @@ typedef int digit;
 typedef int number;
 
 int sum;
+int p;
 // 1121231234
 // ...101112
 struct orig_seq
@@ -21,10 +22,10 @@ struct orig_seq
 // 12345678910
 struct monotonic_seq
 {
-  monotonic_seq(int n, int p) : n(n), p(p)  {}
+  monotonic_seq(int n) : n(n)  {}
   int n; // ostatnia liczba w ciagu (w przykladzie jest to 10
-  int p;
-}ms(0,0);
+  //  int p;
+}ms(0);
 
 int len(int n)
 {
@@ -44,9 +45,9 @@ void test_len()
   assert(len(199) == 3);
 }
 
-monotonic_seq f( position p) // z oryginalnego ciagu tworzymy monotoniczny
+int f( position p) // z oryginalnego ciagu tworzymy monotoniczny
 {
-  int dl = 1;
+  int dl = 2;
   number c1= 0,c2= 1;
   int i;
   while(0==0)
@@ -54,16 +55,28 @@ monotonic_seq f( position p) // z oryginalnego ciagu tworzymy monotoniczny
     if( c1 <= p && p <= c2 )
       break;
     
-    i = c2;    
-    c2 += (c2 - c1) + 1;
+    i = c2;
+    c2 += (c2 - c1) + len(dl);
     c1 = i;
      dl++;
    
   }
-  p-=c1;//pozycja w monotonicznym ciagu
+  p -= c1;
   cout <<"pozycja w monotonicznym ciagu: "<<p<<'\n';
-  cout << "ostatnia liczba w monotonicznym ciagu: "<<c2-c1<<'\n';
-  return monotonic_seq(c2-c1,p);
+  cout << "ilosc cyfr w monotonicznym ciagu: "<<c2-c1<<'\n';
+  //  return c2-c1;
+  return p;
+}
+
+void test_f()
+{
+  assert(f(8)== 2);
+  assert(f(6)== 3);
+  assert(f(26)== 5);
+  assert(f(44)== 8);
+  assert(f(55)== 10);
+  assert(f(56)== 11);
+  assert(f(57)== 1);
 }
 
 int number_longitude(int p)
@@ -75,17 +88,15 @@ int number_longitude(int p)
       i++;
       sum += 9*pow(10,i-1);
     }
-  //  cout <<"pozycja tuz przed odjeciem :"<<p<<'\n';
-  ms.p -= 9*pow(10,i-2);
-  cout <<"dlugosc liczb wsrod ktorych znajduje sie argument: "<<i<<'\n';
-  cout <<"pozycja wsrod liczb i-dlugich: "<<ms.p<<'\n';
+  cout << "int(9*pow(10,i-2): "<<int(9*pow(10,i-2))<<'\n';
+  p -= int(9*pow(10,i-2));
+   cout <<"dlugosc liczb wsrod ktorych znajduje sie argument: "<<i<<'\n';
+   cout <<"pozycja wsrod liczb i-dlugich: "<<p<<'\n';
   return i;
 }
 
 void test_number_longitude()
 {
-  assert(number_longitude(10) == 2);
-  assert(number_longitude(9) == 1);
   assert(number_longitude(99) == 2);
   assert(number_longitude(175) == 3);
   assert(number_longitude(8923) == 4);
@@ -94,14 +105,22 @@ void test_number_longitude()
 int get_number(int p,int i)
 {
   if(i == 1)
-    return floor(p/i);
+    {
+      cout << "floor(p/i): "<<floor(p/i)<<'\n';
+      return floor(p/i);
+    }
   else
-    return floor(p/i) + pow(10,i-1);
+    {
+      cout << "floor(p/i): "<<floor(p/i)<<'\n';
+      cout << pow(10,i-1)<<'\n';
+      return floor(p/i) + pow(10,i-1);
+    }
 }
 
 
 void test_get_number()
 {
+  assert(get_number(45,2) == 9);
   assert(get_number(8,2) == 14);
   assert(get_number(6,1) == 6);
   assert(get_number(2,3) == 100);
@@ -115,21 +134,22 @@ int get_digit()
 
 int read()
 {
-  int p;
-  cin >> p;
-  return p;
+  int g;
+  cin >> g;
+  return g;
 }
 
 int main()
 {
+  //  test_f();
   test_number_longitude();
   test_get_number();
   test_len();
   
   int p = read();
-  ms = f(p);
-  int i = number_longitude(ms.p);
-  cout <<get_digit(get_number(ms.p,i))<<'\n';
+  int position = f(p);
+  int i = number_longitude(p);
+  cout <</*get_digit(*/get_number(p,i)/*)*/<<'\n';
   
   return 0;
 }
