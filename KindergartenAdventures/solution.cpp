62f26e03df519b0b92bda5093070f117ed884f40
rafal <rafpacut@gmail.com>
2018-03-21 13:00:34 +0100

    Template for Hackerranks Kindergarten Adventures problem

---
+++ KindergartenAdventures/solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string t_temp_temp;
+    getline(cin, t_temp_temp);
+
+    vector<string> t_temp = split_string(t_temp_temp);
+
+    vector<int> t(n);
+
+    for (int t_itr = 0; t_itr < n; t_itr++) {
+        int t_item = stoi(t_temp[t_itr]);
+
+        t[t_itr] = t_item;
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
